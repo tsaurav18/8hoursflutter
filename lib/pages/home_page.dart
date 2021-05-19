9e0da1db39001c1914669eeca88e9e74b1ceaf03
tsaurav18 <tsaurav1711@gmail.com>
2021-05-19 10:57:16 +0900

    theme extracted

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -6,7 +6,9 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Catalog"),
+        title: Text(
+          "Catalog",
+        ),
       ),
       body: Center(
         child: Container(
